39098fc0a41a724154d35f01834eb30ef6b7585c
Tobias Bachmann <t.bachmann@taponia.local>
2018-06-04 14:29:26 +0200

    Signed-off-by: Tobias Bachmann <t.bachmann@taponia.local>
    	modified:   Console/Program.cs
    	new file:   Console/Table.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
-
+﻿
 namespace Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
+            Table book = new Table();
+            book.AddData(91);
+            book.AddData(89.5f);
+            //System.Console.WriteLine(89.5);
+
+            book2 = new Table();
+            book.AddData(76);
         }
     }
 }
---
+++ Console/Table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console
+{
+    class Table
+    {
+        public Table()
+        {
+            grades = new List<float>();
+
+        }
+
+
+        public void AddData(float grade)
+        {
+            grades.Add(grade);
+
+        }
+
+        List<float> grades;
+    }
+}
